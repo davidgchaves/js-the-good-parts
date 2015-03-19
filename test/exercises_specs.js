16e526cd276577934c4daf43e24fcb682b4f915d
David G Chaves <davidgchaves@gmail.com>
2015-03-19 18:31:14 +0100

    Ex4: add returns the sum of two numbers

--- test/exercises_specs.js
+++ test/exercises_specs.js
@@ -15,3 +15,13 @@ describe('identity', function() {
   });
 });
 
+/*
+ * Ex4: Write two binary functions add and mul that
+ *      take two numbers and return their sum and product
+ */
+describe('add', function() {
+  it('returns the sum of two numbers', function() {
+    expect(ex.add(3,4)).to.be.equal(7);
+  });
+});
+
