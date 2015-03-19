da9ca32c87bf463a059445742ab15ac3367bd803
David G Chaves <davidgchaves@gmail.com>
2015-03-19 18:17:31 +0100

    Ex3: identity returns its given argument

--- test/exercises_specs.js
+++ test/exercises_specs.js
@@ -3,3 +3,15 @@
 var chai   = require('chai'),
     expect = chai.expect;
 
+var ex = require('../lib/exercises.js');
+
+/*
+ * Ex3: Write an identity function that
+ *      takes an argument and returns the same argument
+ */
+describe('identity', function() {
+  it('returns the given argument', function() {
+    expect(ex.identity(3)).to.be.equal(3);
+  });
+});
+
